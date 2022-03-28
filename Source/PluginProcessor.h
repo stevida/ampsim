34f96984186a5b568b3eebfc10e9644f81c5e008
stevida <vidalsteven001@gmail.com>
2022-03-28 02:26:53 -0400

    added update(), calls updateCoefficients
    eliminated lots of repeated for when updating how many coefficients each slope gets, used a template so it could be call for whatever chain module in what ever channel

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -108,51 +108,54 @@ public:
 
     
     
+    /**
+        REFACTORED THIS FUNCTION TO MAKE THE CODE CLEANER
+     
+     */
+    
+    
+    template<int Index, typename ChainType, typename CoefficientType>
+    void update(ChainType& chain, const CoefficientType& coefficients){
+        updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+        chain.template setBypassed<Index>(false);
+    }
+
+    
     /**using a template so that we can reuse code for each filter, not just the lowpass, instead of having to explicitly type it out for each channel in each filter*/
     template<typename ChainType, typename CoefficientType>
-    void updateCutFilter(ChainType& leftLowCut,
-                         const CoefficientType& cutCoefficients,
-                         //const ChainSettings& chainSettings)
-                        const Slope& lowCutSlope)
+    void updateCutFilter(ChainType& chain,
+                         const CoefficientType& coefficient,
+                         const Slope& slope)
     {
-
-        leftLowCut.template setBypassed<0>(true);
-        leftLowCut.template setBypassed<1>(true);
-        leftLowCut.template setBypassed<2>(true);
-        leftLowCut.template setBypassed<3>(true);
-        
-        switch (lowCutSlope) {
-            case Slope_12:
-                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-                leftLowCut.template setBypassed<0>(false);
-                break;
-            case Slope_24:
-                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-                leftLowCut.template setBypassed<0>(false);
-                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-                leftLowCut.template setBypassed<1>(false);
-                break;
-            case Slope_36:
-                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-                leftLowCut.template setBypassed<0>(false);
-                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-                leftLowCut.template setBypassed<1>(false);
-                *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
-                leftLowCut.template setBypassed<2>(false);
-                break;
-            case Slope_48:
-                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-                leftLowCut.template setBypassed<0>(false);
-                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
-                leftLowCut.template setBypassed<1>(false);
-                *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
-                leftLowCut.template setBypassed<2>(false);
-                *leftLowCut.template get<3>().coefficients = *cutCoefficients[3];
-                leftLowCut.template setBypassed<3>(false);
-                break;
-        }
+        chain.template setBypassed<0>(true);
+        chain.template setBypassed<1>(true);
+        chain.template setBypassed<2>(true);
+        chain.template setBypassed<3>(true);
     
+        
+        switch (slope) {
+            case Slope_48:
+            {
+                update<3>(chain,coefficient);
+            }
+            case Slope_36:
+            {
+                update<2>(chain,coefficient);
+            }
+            case Slope_24:
+            {
+                update<1>(chain,coefficient);
+                break;
+            }
+            case Slope_12:
+            {
+                update<0>(chain,coefficient);
+            }
+        }
+                
+                
     }
+
                                                 
 private:
     //==============================================================================
